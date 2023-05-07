a8662b1706697d76beb1648c50b8963f43fd2e22
coob <koosanghwi@gmail.com>
2023-05-08 00:06:27 +0900

    ver 1.36

--- mymodule/grow_3.py
+++ mymodule/grow_3.py
@@ -138,7 +138,23 @@ def quest_get(cla):
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                 imgs_ = imgs_set_(110, 970, 260, 1030, cla, img, 0.83)
                 if imgs_ is not None and imgs_ != False:
-                    time.sleep(1)
+                    time.sleep(0.5)
+
+                    full_path = "c:\\nightcrow\\imgs\\quest\\sub_not_checked.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(510, 990, 540, 1020, cla, img, 0.83)
+                    if imgs_ is not None and imgs_ != False:
+                        click_pos_reg(imgs_.x, imgs_.y, cla)
+                        time.sleep(0.5)
+
+                    full_path = "c:\\nightcrow\\imgs\\check\\point.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(550, 140, 620, 970, cla, img, 0.8)
+                    if imgs_ is not None and imgs_ != False:
+                        click_pos_reg(imgs_.x - 20, imgs_.y + 5, cla)
+                        time.sleep(0.5)
 
                     full_path = "c:\\nightcrow\\imgs\\quest\\bosang_1.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
