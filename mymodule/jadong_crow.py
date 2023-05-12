a8c08cddcedf099304f94833048651e3f975f9d2
coob <koosanghwi@gmail.com>
2023-05-12 23:35:51 +0900

    ver 1.40

--- mymodule/jadong_crow.py
+++ mymodule/jadong_crow.py
@@ -725,6 +725,17 @@ def go_to_spot(cla):
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                     imgs_ = imgs_set_(820, 880, 910, 970, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
+
+                        full_path = "c:\\nightcrow\\imgs\\grow\\grow_3\\gyunway_bawigil.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(40, 75, 160, 110, cla, img, 0.8)
+                        if imgs_ is not None and imgs_ != False:
+                            pyautogui.keyDown('a')
+                            time.sleep(0.1)
+                            pyautogui.keyUp('a')
+                            print("pyautogui.press('a')")
+
                         full_path = "c:\\nightcrow\\imgs\\jadong\\fly_.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
@@ -732,15 +743,7 @@ def go_to_spot(cla):
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             print("플라잉 부스터!!!")
-                            full_path = "c:\\nightcrow\\imgs\\grow\\grow_3\\gyunway_bawigil.PNG"
-                            img_array = np.fromfile(full_path, np.uint8)
-                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(40, 75, 160, 110, cla, img, 0.8)
-                            if imgs_ is not None and imgs_ != False:
-                                pyautogui.keyDown('a')
-                                time.sleep(0.1)
-                                pyautogui.keyUp('a')
-                                print("pyautogui.press('a')")
+
                     else:
                         full_path = "c:\\nightcrow\\imgs\\jadong\\in_spot_walking_2.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
