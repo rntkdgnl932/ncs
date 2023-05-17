42b82d07caeedce9c489800913a641fdcd0f6fe4
coob <koosanghwi@gmail.com>
2023-05-17 12:10:27 +0900

    ver 1.42

--- mymodule/get_item.py
+++ mymodule/get_item.py
@@ -747,7 +747,7 @@ def guild_check(cla):
                 full_path = "c:\\nightcrow\\imgs\\guild\\guild_giboo_out.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(440, 330, 510, 380, cla, img, 0.8)
+                imgs_ = imgs_set_(400, 900, 700, 1030, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
 
                     full_path = "c:\\nightcrow\\imgs\\guild\\guild_information.PNG"
